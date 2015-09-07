d6a6170fe3b58303720ed3c03b5d60bc9bfedede
cairenhe00 <158164040@qq.com>
2015-09-07 23:11:51 +0800

    commit secTime

---
+++ src/cn/cairenhe/gitTest/gitTest.java
@@ -0,0 +1,10 @@
+package cn.cairenhe.gitTest;
+
+/**
+ * Created by cairenhe on 2015/9/7.
+ */
+public class gitTest {
+    public static void main(String args[]){
+        System.out.println("Hello git");
+    }
+}
